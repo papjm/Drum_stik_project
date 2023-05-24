8b853f329dbcef39733804cabd6b4effb8e5060b
papjm <102404994+papjm@users.noreply.github.com>
2023-05-24 15:41:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-alert(0)
+var numberOfDrumButtons = document.querySelectorAll(".drum").length
+
+for (var i=0; i<numberOfDrumButtons; i++) {
+  document.querySelectorAll("button")[i].addEventListener("click", function(){
+    alert("i got clicked")
+  });
+
+}
